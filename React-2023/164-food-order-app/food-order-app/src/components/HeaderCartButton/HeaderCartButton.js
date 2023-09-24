7da40fdfb15960fd1bd433530f43d416d9465ef7
Hakan <h_ergoksen@hotmail.com>
2023-09-24 14:41:11 +0300

    refactoring

--- React-2023/164-food-order-app/food-order-app/src/components/HeaderCartButton/HeaderCartButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './HeaderCartButton.module.css'
-
-const HeaderCartButton = () =>{
-    return <button className={classes.button}><span className={classes.icon}></span>Button <span className={classes.badge}>1</span></button>
-}
-
-export default HeaderCartButton;
