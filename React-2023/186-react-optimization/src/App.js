7f89367f8fa6fb9ac68f856fd30d8426dbc2de12
Hakan <h_ergoksen@hotmail.com>
2023-10-05 16:43:28 +0300

    testing useCallback

--- React-2023/186-react-optimization/src/App.js
+++ React-2023/186-react-optimization/src/App.js
@@ -1,22 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import Button from "./components/UI/Button/Button";
 import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  console.log('APP is Running!!!');
+  console.log("APP is Running!!!");
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(!allowToggle);
   };
 
+  const toggle = allowToggle ? 'true' : 'false'
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <Button onClick={allowToggleHandler}> Allow Toggle: {toggle} </Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+      {showParagraph && <p>This is new!</p>}
     </div>
   );
 }
